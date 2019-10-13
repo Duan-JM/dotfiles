013dbd8c071f390a474eb62b6737d59a72852e82
Duan-JM <vincent.duan95@bupt.edu.cn>
2019-10-13 11:57:43 +0800

    fix some bug in scripts

--- vim_auto_install.sh
+++ vim_auto_install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # We only support neovim here
-
 if [ $(uname) == "Darwin" ]; then
   echo "Dectect MacOS"
   if command -v brew >/dev/null 2>&1; then 
@@ -16,6 +15,8 @@ if [ $(uname) == "Darwin" ]; then
     echo "neovim detected"
   else
     echo 'no existing neovim, installing'
+    brew install update
+    brew install utf8proc
     brew install --HEAD neovim
   fi
 
@@ -26,11 +27,16 @@ if [ $(uname) == "Darwin" ]; then
     brew install git
   fi
 
-  if command -v pip3 >/dev/null 2>&1; then
+  if command -v pip3 >/dev/null 2>&1 || command -v pip >/dev/null 2>&1; then
     echo 'pip3 detected, skip install pip'
   else
     echo 'no exists pip, installing'
-    sudo apt-get install python3-pip
+    if command -v python3 >/dev/null 2>&1; then
+      python3 get-pip.py
+    fi
+    if command -v python2 >/dev/null 2>&1; then
+      python get-pip.py
+    fi
   fi
 
 
@@ -69,13 +75,26 @@ echo "Add python support"
 pip3 install pynvim
 
 echo "Copy the Configure Files"
+if [ -f "~/.vim" ]; then
+  echo "backup existing vim config file"
+  cp -rf ~/.vim ~/.vimrc_bak
+  echo "deleting existing vim config file"
+  rm -rf ~/.vim
+fi
+
+if [ -f "~/.vimrc" ]; then
+  echo "backup existing vimrc"
+  cp ~/.vimrc ~/.vimrc.bak
+  echo "deleting existing vimrc"
+  rm ~/.vimrc
+fi
 
 cp -rf ./vim ~/.vim
 cp -rf ./vim/vimrc ~/.vimrc
 
 echo "Changing relevant linking"
 
-if [ -d "~/.config/nvim" ]; then
+if [ -f "~/.config/nvim" or -d "~/.config/nvim" ]; then
   echo "deleting existing nvim config file"
   rm -rf ~/.config/nvim
 fi
@@ -90,17 +109,23 @@ ln -sf ~/.vim ~/.config/nvim
 ln -sf ~/.vimrc ~/.config/nvim/init.vim
 
 
-# maychange to install node without sudo
-echo "Install node for coc"
-sudo add-apt-repository ppa:chris-lea/node.js --yes
-sudo apt-get update
-sudo apt-get install nodejs npm --yes
-sudo npm config set registry https://registry.npm.taobao.org
-sudo npm config list
-sudo npm install n -g
-sudo n stable
-sudo node -v
-echo "Install latest node finished"
+if [ $(uname) == "Darwin" ]; then
+  # maychange to install node without sudo
+  brew install --HEAD universal-ctags/universal-ctags/universal-ctags
+
+elif [ $(uname) == "Linux" ]; then
+  echo "Install node for coc"
+  sudo add-apt-repository ppa:chris-lea/node.js --yes
+  sudo apt-get update
+  sudo apt-get install nodejs npm --yes
+  sudo npm config set registry https://registry.npm.taobao.org
+  sudo npm config list
+  sudo npm install n -g
+  sudo n stable
+  sudo node -v
+  echo "Install latest node finished"
+
+fi
 
 echo "Installing ctags for Vista"
 if [ $(uname) == "Darwin" ]; then
@@ -119,6 +144,8 @@ elif [ $(uname) == "Linux" ]; then
   sudo make install
 fi
 
+echo "Installing Pylint autopep8 jedi"
+pip3 install pylint autopep8 jedi
 echo "Install Finished, Please manually change default bash to zsh"
 
 echo "Please Manually Install Plugin in neovim using follow cmds"
