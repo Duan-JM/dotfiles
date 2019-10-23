52638099bd8025e1ab4a655e839372279e179663
Duan-JM <vincent.duan95@bupt.edu.cn>
2019-10-23 17:30:48 +0800

    update auto install tmux

--- tmux_auto_install.sh
+++ tmux_auto_install.sh
@@ -1,3 +1,46 @@
+#!/bin/bash
+
+if [ $(uname) == "Darwin" ]; then
+  echo "Dectect MacOS"
+
+  if command -v brew >/dev/null 2>&1; then 
+    echo 'brew detected, skip install brew' 
+  else 
+    echo 'no exists brew, installing' 
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  fi
+
+  if command -v git >/dev/null 2>&1; then 
+    echo 'git detected, skip install git' 
+  else 
+    echo 'no exists git, installing' 
+    brew install git
+  fi
+
+  if command -v tmux >/dev/null 2>&1; then
+    echo 'tmux detected, skip install tmux'
+  else
+    echo 'no exist tmux, installing'
+    brew install tmux
+  fi
+
+elif [ $(uname) == "Linux" ]; then
+  echo "Detect Linux"
+
+  if command -v git >/dev/null 2>&1; then 
+    echo 'git detected, skip install git' 
+  else 
+    echo 'no exists git, installing' 
+    sudo apt install git
+  fi
+
+  if command -v tmux >/dev/null 2>&1; then
+    echo 'tmux detected, skip install tmux'
+  else
+    echo 'no exist tmux, installing'
+    sudo apt install tmux
+  fi
+
 # simple script
 cp -r ./tmux/tmux.conf ~/.tmux.conf
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
