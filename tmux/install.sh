ae6f6d315c9f6022c89ee35dac3ddf113ba756ce
Duan-JM <vincent.duan95@gmail.com>
2024-08-19 11:14:05 +0800

    fix tmux install bug

--- tmux/install.sh
+++ tmux/install.sh
@@ -3,6 +3,28 @@ SUDO_PREFIX=$1
 SCRIPT=$(readlink -f $0)
 SCRIPTPATH=`dirname $SCRIPT`
 
+#######################################
+# Output error messages with time
+# Globals:
+#   error messages
+# Arguments:
+#   None
+#######################################
+err() {
+  echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: $*" >&2
+}
+
+#######################################
+# Output info messages with time
+# Globals:
+#   info messages
+# Arguments:
+#   None
+#######################################
+info() {
+  echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: $*" >&1
+}
+
 if [ $(uname) == "Darwin" ]; then
   echo "Dectect MacOS"
 
@@ -45,7 +67,24 @@ elif [ $(uname) == "Linux" ]; then
 fi
 
 # simple script
+if [ -d "${HOME}/.tmux/" ]; then
+  info "backup existing tmux config file"
+  cp -rf "${HOME}"/.tmux "${HOME}"/.tmux_bak || ! err "Backup failed interrupt" || exit
+  info "deleting existing tmux config file"
+  rm -rf "${HOME}"/.tmux
+fi
+
+info "Creating the Tmux Configure Files"
 cp -rf ${SCRIPTPATH}/tmux.conf ${HOME}/.tmux.conf
 cp -rf ${SCRIPTPATH}/tmux ${HOME}/.tmux
-git clone https://github.com/tmux-plugins/tpm ${HOME}/.tmux/plugins/tpm
+
+info "Installing the Tmux plugins"
+if [ -d "${HOME}/.tmux/plugins/tpm/" ]; then
+  info "tpm founded, skipping"
+else
+  info "tpm not found, installing tpm."
+  git clone https://github.com/tmux-plugins/tpm ${HOME}/.tmux/plugins/tpm
+fi
+
+info "Installing plugins"
 bash ${HOME}/.tmux/plugins/tpm/bin/install_plugins
