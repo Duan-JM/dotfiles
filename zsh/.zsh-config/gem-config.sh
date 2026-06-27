1fdcf726c01a4d7f71f9b350f75aab75f5f9775d
Duan-JM <vincent.duan95@gmail.com>
2026-06-27 11:01:59 +0800

    optimize zsh and delete sr config

--- zsh/.zsh-config/gem-config.sh
+++ zsh/.zsh-config/gem-config.sh
@@ -1,1 +1,5 @@
-PATH="$(ruby -e 'print Gem.default_dir')/bin:$PATH"
+for gem_bin in "$HOME"/.gem/ruby/*/bin(N) /opt/homebrew/lib/ruby/gems/*/bin(N) /usr/local/lib/ruby/gems/*/bin(N); do
+  [[ -d "$gem_bin" ]] && path=("$gem_bin" $path)
+done
+
+typeset -U path
