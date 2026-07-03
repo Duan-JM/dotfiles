2f8e97247b589e7a420bea8f6b6d30cedba5e462
Duan-JM <vincent.duan95@gmail.com>
2026-07-03 16:22:56 +0800

    zsh: add fzf install scripts

--- zsh/.zsh-config/fzf.sh
+++ zsh/.zsh-config/fzf.sh
@@ -1,5 +1,4 @@
 # config for fzf
-# need to execute $(brew --prefix)/opt/fzf/install
 export FZF_DEFAULT_COMMAND="fd --exclude={.git,.idea,.vscode,.sass-cache,node_modules,build} --type f"
 export FZF_DEFAULT_OPTS="--height 40% --layout=reverse --preview '(highlight -O ansi {} || cat {}) 2> /dev/null | head -500'"
 
@@ -30,4 +29,8 @@ tm() {
   session=$(tmux list-sessions -F "#{session_name}" 2>/dev/null | fzf --exit-0) &&  tmux $change -t "$session" || echo "No sessions found."
 }
 
-[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+if [[ -r ~/.fzf.zsh ]]; then
+  source ~/.fzf.zsh
+elif (( $+commands[fzf] )); then
+  source <(fzf --zsh)
+fi
--- zsh/install.sh
+++ zsh/install.sh
@@ -2,8 +2,35 @@
 SUDO_PREFIX=${1:-sudo}
 SCRIPTPATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
 
+install_fzf() {
+  if command -v fzf >/dev/null 2>&1; then
+    echo 'fzf detected, skip install fzf'
+  else
+    echo 'no exist fzf, installing'
+    if [ "$(uname)" == "Darwin" ]; then
+      HOMEBREW_NO_AUTO_UPDATE=1 HOMEBREW_NO_INSTALL_CLEANUP=1 brew install --quiet fzf
+    elif [ "$(uname)" == "Linux" ]; then
+      ${SUDO_PREFIX} apt install --yes fzf
+    fi
+  fi
+
+  if [ ! -d "${HOME}/.fzf" ]; then
+    echo 'no exist fzf shell integration, installing'
+    git clone --depth 1 https://github.com/junegunn/fzf.git "${HOME}/.fzf"
+  else
+    echo 'fzf shell integration detected, skip clone'
+  fi
+
+  if [ -x "${HOME}/.fzf/install" ]; then
+    "${HOME}/.fzf/install" --all --no-bash --no-fish --no-update-rc
+  else
+    echo "fzf installer not found at ${HOME}/.fzf/install"
+    return 1
+  fi
+}
+
 # os check
-if [ $(uname) == "Darwin" ]; then
+if [ "$(uname)" == "Darwin" ]; then
   echo "Dectect MacOS"
 
   if command -v brew >/dev/null 2>&1; then
@@ -30,12 +57,12 @@ if [ $(uname) == "Darwin" ]; then
 
   if [ ! -d "${HOME}/.oh-my-zsh" ]; then
     echo 'no exist oh my zsh, installing'
-    git clone https://github.com/robbyrussell/oh-my-zsh.git ${HOME}/.oh-my-zsh
+    git clone https://github.com/robbyrussell/oh-my-zsh.git "${HOME}/.oh-my-zsh"
   else
     echo 'oh my zsh detected, skip installing'
   fi
 
-elif [ $(uname) == "Linux" ]; then
+elif [ "$(uname)" == "Linux" ]; then
   echo "Detect Linux"
 
   if command -v git >/dev/null 2>&1; then
@@ -55,7 +82,7 @@ elif [ $(uname) == "Linux" ]; then
 
   if [ ! -d "${HOME}/.oh-my-zsh" ]; then
     echo 'no exist oh my zsh, installing'
-    git clone https://github.com/robbyrussell/oh-my-zsh.git ${HOME}/.oh-my-zsh
+    git clone https://github.com/robbyrussell/oh-my-zsh.git "${HOME}/.oh-my-zsh"
   else
     echo 'oh my zsh detected, skip installing'
   fi
@@ -64,17 +91,19 @@ else
   echo "Scripts do not support $(uname)"
 fi
 
+install_fzf
+
 echo "Copy the Configure Files"
 
 cp "${SCRIPTPATH}/zshrc" "${HOME}/.zshrc"
 cp -rf "${SCRIPTPATH}/.zsh-config" "${HOME}/.zsh-config"
 
 echo "Installing Plugins"
-git clone https://github.com/zsh-users/zsh-syntax-highlighting ${HOME}/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-autosuggestions ${HOME}/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting "${HOME}/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting"
+git clone https://github.com/zsh-users/zsh-autosuggestions "${HOME}/.oh-my-zsh/custom/plugins/zsh-autosuggestions"
 
 echo "Installing Themes"
 git clone https://github.com/iplaces/astro-zsh-theme.git /tmp/astro-zsh-theme
-cp /tmp/astro-zsh-theme/astro.zsh-theme ${HOME}/.oh-my-zsh/themes/
+cp /tmp/astro-zsh-theme/astro.zsh-theme "${HOME}/.oh-my-zsh/themes/"
 
 echo "Install Finished, Please manually change default bash to zsh"
